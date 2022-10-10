fa62d8540dcb02fd1a7e7582cc5a8c00248a34e4
Tiphanie <108735280+Tiphanie92@users.noreply.github.com>
2022-10-10 08:35:37 +0200

    Modifications

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,49 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://Tiphanie:gLwfgpJcf73uoYRf@cluster0.ahmnscv.mongodb.net/?retryWrites=true&w=majority",
-    {
-      userNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée"));
+// importer { MongoClient } depuis 'mongodb'
+const { MongoClient } = require("mongodb");
 
-app.use((req, res) => {
-  res.json({ message: "requête bien reçue" });
+// URL de connexion
+const url =
+  "mongodb+srv://Tiphanie:TqRGKOqNFkwygbba@cluster0.ahmnscv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+// Nom de la base de donnée
+const dbName = "test";
+
+async function main() {
+  // Utilisation de la methode connect pour se connecter au serveur
+  await client.connect();
+  console.log("Connection a Mongodb réussie");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+  return "terminé.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
 });
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json("La requête a bien été reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
+
 module.exports = app;
